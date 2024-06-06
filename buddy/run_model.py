79664ea657b9f9d0b753e7257a9aeb7f8610b408
Brieuc de Ghellinck <brieucdg@gmail.com>
2024-06-06 17:06:42 +0200

    load/save on GCP

--- buddy/run_model.py
+++ buddy/run_model.py
@@ -1,11 +1,20 @@
+import time
+import os
 import pandas as pd
+import joblib
+import glob
+
 from sklearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from preprocessing import transform_input
-import joblib
 from sklearn.linear_model import SGDClassifier
+from google.cloud import storage
+
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+LOCAL_REGISTRY_PATH = os.environ.get("LOCAL_REGISTRY_PATH")
 
 def naive_bayes_model():
     # Pipeline vectorizer + Naive Bayes
@@ -31,9 +40,9 @@ def train_model (data, model):
     y = data["target"]
 
     # Fit model
-    print("Fitting Model ...")
+    print("\nFitting Model ...")
     trained_model = model.fit(X,y)
-    print("Model trained ...")
+    print("\n✅ Model trained ...")
     return trained_model
 
 def predict_model (txt: str, trained_model):
@@ -42,14 +51,69 @@ def predict_model (txt: str, trained_model):
     return y_pred
 
 def save_model (model, model_name):
-    print("Saving Model ...")
-    joblib.dump(model, f'models/{model_name}_model.pkl')
+
+    print("\nSaving Model ...")
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    # Save model locally
+    model_path = f'{LOCAL_REGISTRY_PATH}/{model_name}_model_{timestamp}.pkl'
+    joblib.dump(model, model_path)
+    print("\n✅ Model saved locally")
+
+    # Save model onn GCS
+    if MODEL_TARGET == "gcs":
+        model_filename = model_path.split("/")[-1]
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(f"{LOCAL_REGISTRY_PATH}/{model_filename}")
+        blob.upload_from_filename(model_path)
+
+        print("\n✅ Model saved to GCS")
+    else :
+        print("\n❌ Model not saved")
+    return None
 
 def load_model (model_name):
-    print("Loading Model ...")
-    model = joblib.load(f"models/{model_name}_model.pkl")
-    print("Model loaded...")
-    return model
+
+    if MODEL_TARGET == "gcs":
+        print("\nLoading Model on GCS ...")
+        client = storage.Client()
+        # Get a list of all the blobs in the bucket with the specified prefix
+        blobs = list(client.get_bucket(BUCKET_NAME).list_blobs(prefix=f"{LOCAL_REGISTRY_PATH}/{model_name}_model_"))
+
+        try:
+            # Sort the blobs by their creation time and get the latest one
+            latest_blob = max(blobs, key=lambda x: x.updated)
+
+            # Download the latest blob to a local file
+            latest_model_path_to_save = os.path.join(LOCAL_REGISTRY_PATH, latest_blob.name.split("/")[-1])
+            latest_blob.download_to_filename(latest_model_path_to_save)
+
+            latest_model = joblib.load(latest_model_path_to_save)
+
+            print("\n✅ Latest model downloaded from cloud storage")
+
+            return latest_model
+
+        except:
+            print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
+
+            return None
+
+    elif MODEL_TARGET == "local":
+        print("\n Loading model locally ...")
+        # Get the latest model version name by the timestamp on disk
+        local_model_paths = glob.glob(f"{LOCAL_REGISTRY_PATH}/{model_name}_model*.pkl")
+
+        if not local_model_paths:
+            return None
+        most_recent_model_path = sorted(local_model_paths)[-1]
+
+        latest_model = joblib.load(most_recent_model_path)
+        print("✅ Model loaded locally...")
+        return latest_model
+    else:
+        return None
 
 if __name__ == "__main__":
     model, model_name = sgd_model()
@@ -61,6 +125,6 @@ if __name__ == "__main__":
         model_name
         )
     model_loaded = load_model(model_name)
-    assert predict_model("I'm super happy",model_loaded) == 0, " Text:'I'm super happy' should be equals to 0 "
-    assert predict_model("I wanna kill myself",model_loaded) == 1, " Text: 'I wanna kill myself' should be equals to 1 "
-    print ("Model does work fine!")
+    assert predict_model("I'm super happy",model_loaded) == 0, "\n ❌ Text:'I'm super happy' should be equals to 0 "
+    assert predict_model("I wanna kill myself",model_loaded) == 1, "\n ❌ Text: 'I wanna kill myself' should be equals to 1 "
+    print ("\n✅ Model does work fine!")
