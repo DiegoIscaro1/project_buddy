63792ba8d42f8abbd188a8f81eafd79bddf817be
Brieuc de Ghellinck <brieucdg@gmail.com>
2024-06-06 10:09:10 +0200

    delete unecessary files

--- buddy/load_model.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-def load_model (model_name):
-    print("Loading Model ...")
-    model = joblib.load(f"../models/{model_name}_model.pkl")
-    print("Model loaded...")
-    return model
