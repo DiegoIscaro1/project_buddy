236cc0b3772f7d9fe11894ccfd1b41d0776270b8
Gary Goldblatt <goldblatt.gary@gmail.com>
2024-06-11 10:01:44 +0200

    changed output

--- buddy/main.py
+++ buddy/main.py
@@ -1,18 +1,21 @@
 from buddy.run_model import model_predicting, load_model
 
 
-def predict(input):
-    model_name = "log_reg"
+def predict(input: str)->int:
+    model_name = "log_reg" # Choice of the model
     output = model_predicting(input,load_model(model_name),model_name)
-    if output <= 0.3:
-        result = "This person is going fine!"
-    elif output <= 0.7:
-        result = "There are some concerns but it is still ok"
+    if output <= 0.25:
+        result = 0
+    elif output <= 0.5:
+        result = 1
+    elif output <= 0.80:
+        result = 2
     else:
-        result = "This person is going to commit SUICIDE!!!"
+        result = 3
+
     return result
 
 if __name__ == "__main__":
-    input = "I feel sad. I will get better if people help me"
-    output = predict(input)
-    print(output)
+    input = "There's a constant weight on my chest, and I can't seem to shake it off."
+    result, output = predict(input)
+    print(result)
